7082aeaab824d0bccdb1f0fa9d74505f933ccfb4
Kajanan02 <Kajanan02000@gmail.com>
2024-06-22 22:20:14 +0530

    Login add

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,23 @@ import User from "../modals/userModal.js";
 import generateToken from "../utils/generateToken.js";
 import bcrypt from "bcryptjs";
 import UserOTPVerification from "../modals/UserOTPVerification.js";
-import senEmail from "../utils/email-service/email.js";
+import sendEmail from "../utils/email-service/email.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     const {
-        firstName, lastName, email, password, role, contactNo, gender, address, nicNo, nicFront, nicBack, isVerified
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        contactNo,
+        gender,
+        address,
+        nicNo,
+        nicFront,
+        nicBack,
+        isVerified,
+        profilePic
     } = req.body;
     const userExists = await User.findOne({email})
     if (userExists) {
@@ -16,9 +28,30 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const user = await User.create({
-        firstName, lastName, email, password, role, contactNo, gender, address, nicNo, nicFront, nicBack, isVerified
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        contactNo,
+        gender,
+        address,
+        nicNo,
+        nicFront,
+        nicBack,
+        isVerified,
+        profilePic
     }).then((user) => {
         sendOTP(user, res)
+
+        res.status(201).json({
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            role: user.role,
+            contactNo: user.contactNo
+        })
     }).catch((error) => {
         res.status(400);
         throw new Error(error)
@@ -27,76 +60,101 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 const sendOTP = asyncHandler(async ({_id, email}, res) => {
-
-    console.log(_id, email, "sendOTP")
+    console.log(_id, email, "sendOTP");
     try {
         const id = _id;
         const otp = Math.floor(100000 + Math.random() * 900000);
-        const subject = "Email Verification OTP from Bodimate"
-        const body = `<h1>Your OTP is ${otp}</h1>`
+        const subject = "Email Verification OTP from Bodimate";
+        const body = `<h1>Your OTP is ${otp}</h1>`;
 
         const salt = await bcrypt.genSalt(10);
         const hashedOTP = await bcrypt.hash(otp.toString(), salt);
 
+        const currentTime = new Date();
+        const expirationTime = new Date(currentTime.getTime() + 5 * 60 * 1000); // 5 minutes in the future
+
         const newUserOTPVerification = new UserOTPVerification({
             userId: id,
             otp: hashedOTP,
-            createdAt: Date.now(),
-            expiredAt: Date.now() + 5 * 60 * 1000
-        })
+            email,
+            createdAt: currentTime,
+            expiredAt: expirationTime
+        });
 
+        console.log(`OTP: ${otp}, CreatedAt: ${currentTime.toISOString()}, ExpiredAt: ${expirationTime.toISOString()}`);
         await newUserOTPVerification.save();
-        await senEmail(email, subject, body);
+        await sendEmail(email, subject, body);
 
-        res.status(200).json({
-            status: "PENDING",
-            message: "OTP sent successfully",
-            messageUser: "User created successfully, Please verify your email",
-            data: {userId: id, email: email}
-        })
     } catch (error) {
-        res.status(400);
-        throw new Error(error)
+        res.status(400).json({
+            status: "FAILED",
+            message: "Error sending OTP",
+            error: error.message
+        });
     }
+});
 
-})
 
 const verifyOTP = asyncHandler(async (req, res) => {
+    let userId;
 
-    const {userId, otp} = req.body;
-    console.log(userId, otp, "verifyOTP")
-    try {
-        const userOTPVerification = await UserOTPVerification.findOne({userId})
-        if (!userOTPVerification) {
-            res.status(400).json({status: "FAILED", message: "OTP not found", data: {userId: userId}})
+    if (req.body.userId) {
+        userId = req.body.userId;
+    } else if (req.user && req.user._id) {
+        userId = req.user._id;
+    } else if (req.body.email) {
+        const user = await User.findOne({email: req.body.email});
+        if (!user) {
+            return res.status(400).json({status: "FAILED", message: "No user found", data: {userId}});
         } else {
-            if (userOTPVerification.expiredAt < Date.now()) {
-                res.status(400).json({status: "FAILED", message: "OTP expired", data: {userId: userId}})
-            } else {
-                if (userOTPVerification) {
-                    const isMatch = await bcrypt.compare(otp.toString(), userOTPVerification.otp);
-                    if (isMatch) {
-                        const user = await User.findById(userId)
-                        user.isEmailVerified = true;
-                        await user.save();
-                        res.status(200).json({
-                            status: "SUCCESS",
-                            message: "OTP verified successfully",
-                            data: {userId: userId}
-                        })
-                    } else {
-                        res.status(400).json({status: "FAILED", message: "Invalid OTP", data: {userId: userId}})
-                    }
-                } else {
-                    res.status(400).json({status: "FAILED", message: "OTP not found", data: {userId: userId}})
-                }
-            }
+            userId = user._id;
+        }
+    } else {
+        return res.status(400).json({status: "FAILED", message: "No user found", data: {userId}});
+    }
+
+    const {otp} = req.body;
+    console.log(userId, otp, "verifyOTP");
+
+    try {
+        const userOTPVerification = await UserOTPVerification.findOne({userId});
+        if (!userOTPVerification) {
+            return res.status(400).json({status: "FAILED", message: "OTP not found", data: {userId}});
+        }
+
+        console.log(userOTPVerification.expiredAt)
+
+        const currentTime = new Date(userOTPVerification.createdAt);
+        const expiredAt = new Date(userOTPVerification.expiredAt);
+
+        if (expiredAt < currentTime) {
+            return res.status(400).json({status: "FAILED", message: "OTP expired", data: {userId}});
+        }
+
+        if (userOTPVerification.isExpired) {
+            return res.status(400).json({status: "FAILED", message: "OTP expired", data: {userId}});
+        }
+
+        const isMatch = await bcrypt.compare(otp.toString(), userOTPVerification.otp);
+        if (isMatch) {
+            const user = await User.findById(userId);
+            user.isEmailVerified = true;
+            await user.save();
+            userOTPVerification.isExpired = true;
+            await userOTPVerification.save();
+            return res.status(200).json({
+                status: "SUCCESS",
+                message: "OTP verified successfully",
+                data: {userId}
+            });
+        } else {
+            return res.status(400).json({status: "FAILED", message: "Invalid OTP", data: {userId}});
         }
     } catch (error) {
-        res.status(400);
-        throw new Error(error)
+        res.status(500).json({status: "FAILED", message: "Server error", error: error.message});
     }
-})
+});
+
 
 const loginUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body;
@@ -117,10 +175,109 @@ const loginUser = asyncHandler(async (req, res) => {
             nicBack: user.nicBack,
             isVerified: user.isVerified,
             isEmailVerified: user.isEmailVerified,
+            profilePic: user.profilePic,
             token: token
         })
+    } else {
+        res.status(401)
+        throw new Error("Invalid email or password")
     }
 })
 
-export {registerUser, verifyOTP, loginUser}
+const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    })
+    res.status(200).json({message: "User logged out successfully"})
+
+})
+
+const forgotPassword = asyncHandler(async (req, res) => {
+    const {email} = req.body;
+    const user = await User.findOne({email});
+    if (user) {
+        sendOTP(user, res);
+        res.status(200).json({message: "OTP sent successfully"})
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+
+const resetPassword = asyncHandler(async (req, res) => {
+    const {email, otp, password} = req.body;
+    const userOTPVerification = await UserOTPVerification.findOne({email})
+
+    if (userOTPVerification.expiredAt < Date.now()) {
+        return res.status(400).json({status: "FAILED", message: "OTP expired", data: {email}});
+    } else if (userOTPVerification.isExpired) {
+        return res.status(400).json({status: "FAILED", message: "OTP expired", data: {email}});
+    } else if (userOTPVerification) {
+        const isMatch = await bcrypt.compare(otp.toString(), userOTPVerification.otp);
+        if (isMatch) {
+
+            const user = await User.findById(userOTPVerification.userId)
+            user.password = password;
+            userOTPVerification.isExpired = true;
+            await userOTPVerification.save();
+            await user.save();
+            res.status(200).json({message: "Password reset successfully"})
+        } else {
+            res.status(400)
+            throw new Error("Invalid OTP")
+        }
+    } else {
+        res.status(400)
+        throw new Error("OTP not found")
+    }
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.firstName = req.body.firstName || user.firstName;
+        user.lastName = req.body.lastName || user.lastName;
+        user.email = req.body.email || user.email;
+        user.role = req.body.role || user.role;
+        user.contactNo = req.body.contactNo || user.contactNo;
+        user.gender = req.body || user.gender;
+        user.address = req.body.address || user.address;
+        user.nicNo = req.body.nicNo || user.nicNo;
+        user.nicFront = req.body.nicFront || user.nicFront;
+        user.nicBack = req.body.nicBack || user.nicBack;
+        user.profilePic = req.body.profilePic || user.profilePic;
+        user.isVerified = req.body.isVerified || user.isVerified;
+        user.isEmailVerified = req.body.isEmailVerified || user.isEmailVerified;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email,
+            role: updatedUser.role,
+            contactNo: updatedUser.contactNo,
+            gender: updatedUser.gender,
+            address: updatedUser.address,
+            nicNo: updatedUser.nicNo,
+            nicFront: updatedUser.nicFront,
+            nicBack: updatedUser.nicBack,
+            profilePic: updatedUser.profilePic,
+            isVerified: updatedUser.isVerified,
+            isEmailVerified: updatedUser.isEmailVerified,
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+
+export {registerUser, verifyOTP, loginUser, logoutUser, forgotPassword, resetPassword}
 
