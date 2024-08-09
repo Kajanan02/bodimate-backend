14a1a4536d473b8483766efa477dda726210c81c
Kajanan02 <Kajanan02000@gmail.com>
2024-08-09 10:50:48 +0530

    vercel deploy

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ import contactUsRoutes from "./contactUsRoutes.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.json({msg: "API is running"});
+});
+
 router.use("/api/users", userRoutes);
 router.use("/api/boardings", boardingRoutes);
 router.use("/api/contactUs", contactUsRoutes);
